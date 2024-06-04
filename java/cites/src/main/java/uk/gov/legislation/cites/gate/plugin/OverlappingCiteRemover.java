a2c93665ad1218ebadae0a8f23012f4ea83395e7
Jim Mangiafico <jim@jurisdatum.com>
2024-06-04 15:10:06 -0400

    correct features of certain cites

--- java/cites/src/main/java/uk/gov/legislation/cites/gate/plugin/OverlappingCiteRemover.java
+++ java/cites/src/main/java/uk/gov/legislation/cites/gate/plugin/OverlappingCiteRemover.java
@@ -2,17 +2,22 @@ package uk.gov.legislation.cites.gate.plugin;
 
 import gate.Annotation;
 import gate.AnnotationSet;
+import gate.FeatureMap;
 import gate.annotation.AnnotationSetImpl;
 import gate.creole.metadata.CreoleResource;
 import uk.gov.legislation.cites.gate.CiteEnricher;
 
 import java.util.Iterator;
+import java.util.function.Predicate;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 @CreoleResource(name = "Overlapping Cite Remover", comment = "remove new Cite if it overlaps with old one")
 public class OverlappingCiteRemover extends gate.creole.AbstractLanguageAnalyser implements gate.LanguageAnalyser {
 
     private final Logger logger = Logger.getAnonymousLogger();
+
+    private final Predicate<String> IsYear = Pattern.compile("^\\d{4}$").asMatchPredicate();
 
     @Override
     public void execute() {
@@ -26,6 +31,31 @@ public class OverlappingCiteRemover extends gate.creole.AbstractLanguageAnalyser
             if (overlappingOldCites.isEmpty()) {
                 logger.info("found new cite: " + gate.Utils.stringFor(document, newCite));
                 continue;
+            }
+            if (overlappingOldCites.size() == 1) {
+                Annotation oldCite = overlappingOldCites.inDocumentOrder().get(0);
+                FeatureMap oldFeatures = oldCite.getFeatures();
+                FeatureMap newFeatures = newCite.getFeatures();
+                String oldYear = (String) oldFeatures.get("Year");
+                if (oldYear == null || !IsYear.test(oldYear)) { // uksi/2020/1528
+                    logger.warning("old year is invalid: " + oldYear);
+                    String oldType = (String) oldFeatures.get("Class");
+                    Object oldNumber = oldFeatures.get("Number");
+                    String newType = (String) newFeatures.get("Class");
+                    Object newYear = newFeatures.get("Year");
+                    Object newNumber = newFeatures.get("Number");
+                    if (oldType.equals(newType) || (oldType.startsWith("European") && newType.startsWith("European"))) { // ssi/2005/311
+                        oldFeatures.put("Year", newYear);
+                        oldFeatures.put("Number", newNumber);
+                        oldFeatures.remove("URI");
+                        if (!oldType.equalsIgnoreCase(newType))
+                            logger.info("changing type from " + oldType + " to " + newType);
+                        logger.info("changing year from " + oldYear + " to " + newYear);
+                        if (oldNumber == null)
+                            oldNumber = "<empty>";
+                        logger.info("changing number from " + oldNumber + " to " + newNumber);
+                    }
+                }
             }
             toRemove.add(newCite);
             logRemoval(newCite, overlappingOldCites);
@@ -48,12 +78,12 @@ public class OverlappingCiteRemover extends gate.creole.AbstractLanguageAnalyser
             logger.warning("new year: " + newYear + " -- old year: " + oldYear);
 
         String newNumber = (String) newCite.getFeatures().get("Number").toString().replaceFirst("^0+(?!$)", "");
-        Object oldNumberObject = (String) oldCite.getFeatures().get("Number");
+        Object oldNumberObject = oldCite.getFeatures().get("Number");
         if (oldNumberObject == null) {
             logger.warning("new number: " + newNumber + " -- old number: null");
             return;
         }
-        String oldNumber = (String) oldCite.getFeatures().get("Number").toString().replaceFirst("^0+(?!$)", "");
+        String oldNumber = (String) oldNumberObject.toString().replaceFirst("^0+(?!$)", "");
         if (!newNumber.equals(oldNumber))
             logger.warning("new number: " + newNumber + " -- old number: " + oldNumber);
     }
