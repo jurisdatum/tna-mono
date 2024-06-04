66b0fa6f8e85d15a45b0f7d778d7d8af2ef4cf97
Jim Mangiafico <jim@jurisdatum.com>
2024-06-04 15:18:37 -0400

    refactor cite enricher

--- java/cites/src/main/java/uk/gov/legislation/cites/gate/CiteEnricher.java
+++ java/cites/src/main/java/uk/gov/legislation/cites/gate/CiteEnricher.java
@@ -22,17 +22,21 @@ public class CiteEnricher {
     private final GateXMLPreparer preparer = new GateXMLPreparer(beautifier.getProcessor());
     private final GateArtifactRemover artifactRemover = new GateArtifactRemover(beautifier.getProcessor());
 
+    static void init() throws GateException {
+        if (Gate.isInitialised())
+            return;
+        Gate.init();
+        // this prevents the adding of spaces in the XML
+        Gate.getUserConfig().put(GateConstants.DOCUMENT_ADD_SPACE_ON_UNPACK_FEATURE_NAME, Boolean.FALSE);
+        // add ANNIE
+        Gate.getCreoleRegister().registerPlugin(new Plugin.Directory(CiteEnricher.class.getResource("/annie/")));
+        // add custom plugin
+        // can't use Gate.getCreoleRegister().registerComponent() -- or Plugin.Component() -- they allow only one resource class
+        Gate.getCreoleRegister().registerPlugin( new CustomPlugin());
+    }
+
     public CiteEnricher() throws GateException {
-        if (!Gate.isInitialised()) {
-            Gate.init();
-            // this prevents the adding of spaces in the XML
-            Gate.getUserConfig().put(GateConstants.DOCUMENT_ADD_SPACE_ON_UNPACK_FEATURE_NAME, Boolean.FALSE);
-            // add ANNIE
-            Gate.getCreoleRegister().registerPlugin(new Plugin.Directory(getClass().getResource("/annie/")));
-            // add custom plugin
-            // can't use Gate.getCreoleRegister().registerComponent() -- or Plugin.Component() -- they allow only one resource class
-            Gate.getCreoleRegister().registerPlugin( new CustomPlugin());
-        }
+        init();
 
         sac = (SerialAnalyserController) Factory.createResource("gate.creole.SerialAnalyserController");
 
@@ -48,20 +52,21 @@ public class CiteEnricher {
         sac.setCorpus(Factory.newCorpus("Corpus"));
     }
 
-    public byte[] enrich(byte[] clml) throws IOException, ResourceInstantiationException, ExecutionException {
-        clml = beautifier.transform(clml);
-        clml = preparer.prepare(clml);
+    public byte[] enrich(byte[] clml) throws ResourceInstantiationException, ExecutionException {
         Document doc = loadXml(clml);
-        String enriched1 = enrich(doc);
+        enrich(doc);
+        String enriched1 = serialize(doc);
         byte[] withGateArtifactsRemoved = artifactRemover.remove(enriched1);
         return withGateArtifactsRemoved;
     }
 
-    static Document loadXml(byte[] xml) throws ResourceInstantiationException {
+    public Document loadXml(byte[] xml) throws ResourceInstantiationException {
+        xml = beautifier.transform(xml);
+        xml = preparer.prepare(xml);
         String string = new String(xml, StandardCharsets.UTF_8);
         return loadXml(string);
     }
-    static Document loadXml(String xml) throws ResourceInstantiationException {
+    private static Document loadXml(String xml) throws ResourceInstantiationException {
         FeatureMap params = Factory.newFeatureMap();
         params.put(Document.DOCUMENT_MIME_TYPE_PARAMETER_NAME, "application/xml");
         params.put(Document.DOCUMENT_STRING_CONTENT_PARAMETER_NAME, xml);
@@ -71,15 +76,21 @@ public class CiteEnricher {
         return (Document) Factory.createResource("gate.corpora.DocumentImpl", params);
     }
 
-    private String enrich(Document doc) throws ExecutionException {
-        sac.getCorpus().add(doc);
-        sac.execute();
-        String enriched = serialize(doc);
-        sac.getCorpus().remove(doc);
-        return enriched;
+    public static void removeOriginal(Document doc) {
+        AnnotationSet markup = doc.getAnnotations(OriginalMarkups);
+        AnnotationSet full = markup.get("Citation");
+        markup.removeAll(full);
+        AnnotationSet sub = markup.get("CitationSubRef");
+        markup.removeAll(sub);
     }
 
-    private String serialize(Document doc) {
+    public void enrich(Document doc) throws ExecutionException {
+        sac.getCorpus().add(doc);
+        sac.execute();
+        sac.getCorpus().remove(doc);
+    }
+
+    public static String serialize(Document doc) {
         AnnotationSet newMarkups = doc.getAnnotations(NewMarkups);
         return doc.toXml(newMarkups, true);
     }
